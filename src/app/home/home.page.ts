d82de798f2425475799e2ad77cf9980eb8f7949e
Liam DeBeasi <liamdebeasi@icloud.com>
2020-03-30 15:38:54 +0000

    added reproduction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('slides', { static: true }) slides: any;
+  
+  public slidesArray: number[] = [1,2,3];
+  
+  public counter: number = 100;
 
   constructor() {}
+  
+  reachStart() {
+    if (this.counter <= 0) {
+      console.warn('Caught infinite loop');
+      debugger;
+    }
+    console.log('Reached start',this.slides);
+    this.slidesArray.push(this.slidesArray.length);
+    
+    this.slides.slideTo(1);
+    this.counter--;
+  }
+  
+  reachEnd() {
+    console.log('Reached end');
+    this.slides.slideTo(1);
+  }
 
 }
